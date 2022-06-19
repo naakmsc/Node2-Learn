6a44b87ed08588c61be5f34a0dd85a4de6308b7e
Isai Gutierrez <isaigutierrez125@gmail.com>
2022-06-19 15:41:08 -0600

    Learn about Modules: export, import and the different ways for using

---
+++ demo-modules.js
@@ -0,0 +1,12 @@
+import { a as a1, firstName as lname, add, employee} from "./modules.js";
+
+import c from "./modules.js";
+let log = console.log;
+
+log(c);
+
+log(a1);
+log(lname);
+log(add(5,5));
+log(employee);
+
---
+++ modules.js
@@ -0,0 +1,26 @@
+// export let a = 10;
+
+// export let firstName = "Jairo";
+
+// export let add = (a,b) => a+b;
+
+// export let employee ={
+//     eid: 1,
+//     ename: "Jairo"
+// }
+
+let a = 10;
+
+let firstName = "Jairo";
+
+let add = (a,b) => a+b;
+
+let employee ={
+    eid: 1,
+    ename: "Jairo"
+}
+
+export {a, firstName, add, employee};
+
+let b = 900;
+export default b;
